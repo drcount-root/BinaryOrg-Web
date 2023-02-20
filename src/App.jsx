cd189ebe800b1b737f8e014bd361440f88b3cf6f
Subham Sahu <about.subhamsahu@gmail.com>
2023-02-20 20:32:13 +0530

    tailwind_css added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { Fragment } from "react";
 
 export default function App() {
   return (
-    <div>App</div>
-  )
+    <Fragment>
+      <h3 className="text-center text-9xl">Tailwind CSS Added</h3>
+    </Fragment>
+  );
 }
