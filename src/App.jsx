9b5aee608ab916d49e064ed5a376c6d441a98bbd
Subham Sahu <about.subhamsahu@gmail.com>
2023-02-20 21:26:16 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React, { Fragment } from "react";
+import NavBar from "./components/NavBar";
 
 export default function App() {
   return (
     <Fragment>
-      <h3 className="text-center text-9xl">Tailwind CSS Added</h3>
+      <NavBar />
+      <h3 className="text-center text-6xl mt-32 w-[100%]">Tailwind CSS Added</h3>
     </Fragment>
   );
 }
